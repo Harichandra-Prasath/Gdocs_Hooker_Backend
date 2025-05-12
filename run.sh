a6eef48ffe13122b5e77c5cf72f408448469b9dd
Harichandra Prasath <1hcp302004@gmail.com>
2025-05-12 20:46:43 +0530

    Runner script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Activate the virutal environment
+source .venv/bin/activate
+
+# Get all the env files
+env_files=$(ls ./*.env)
+
+i=0
+for env_file in $env_files
+do
+	python3 main.py --env-file "$env_file" >> ~/.gdocks_hooker_"${i}".logs 2>&1 &
+	((i++))
+done
